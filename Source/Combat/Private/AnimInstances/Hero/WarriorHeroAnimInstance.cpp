6b4c1cc7b1d7226bbdbcbf84d250cf01c0412403
Weicheng Zhu <weicheng.zhu@icloud.com>
2025-01-29 14:53:19 -0500

    17. add relax state

--- Source/Combat/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
+++ Source/Combat/Private/AnimInstances/Hero/WarriorHeroAnimInstance.cpp
@@ -3,3 +3,28 @@
 
 #include "AnimInstances/Hero/WarriorHeroAnimInstance.h"
 
+#include "Characters/WarriorHeroCharacter.h"
+
+void UWarriorHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	if (OwningCharacter)
+	{
+	  OwningHeroCharacter =	Cast<AWarriorHeroCharacter>(OwningHeroCharacter);
+	}
+}
+
+void UWarriorHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	if (BHasAcceleration)
+	{
+		IdleElpasedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		 IdleElpasedTime += DeltaSeconds;
+		 bShouldEnterRelaxState = (IdleElpasedTime >= EnterRelaxStateThreshold);
+	}
+}
